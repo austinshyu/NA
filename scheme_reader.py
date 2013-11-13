5770b5c0c360eb90c0f1376495f19ddd0dce4e2e
BoYoungHwang <by.hwang@berkeley.edu>
2013-11-12 17:54:48 -0800

    comments updated

--- scheme_reader.py
+++ scheme_reader.py
@@ -167,13 +167,12 @@ def read_tail(src):
             src.pop()
             return nil
         if src.current() == ".":
-            src.pop()
-            a = scheme_read(src)
-            if src.current() == ")":
-                src.pop()
+            src.pop()   # Remove dot
+            a = scheme_read(src)   # Read the expression after the dot. ('a' is bound to the first element after the dot.)
+            if src.current() == ")":   # When only one element follows after dot
                 return a
             else:
-                raise SyntaxError("More than one element after dot")
+                raise SyntaxError("More than one element after dot")   # Raise an error if more than one element follow the dot
         first = scheme_read(src)
         rest = read_tail(src)
         return Pair(first, rest)
