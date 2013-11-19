290dbe18694c61970ac1a24cc9c88f0645e9e6ad
austinshyu <austinshyu@berkeley.edu>
2013-11-19 15:59:58 -0800

    minor changes to do_and_form

--- scheme.py
+++ scheme.py
@@ -292,7 +292,7 @@ def do_and_form(vals, env):
     if vals == nil: # Check if no arguments
         return True
     while i < len(vals) and all_true == True: # Iterate through values
-        all_true = scheme_true(scheme_eval(vals[i],eval))
+        all_true = scheme_true(scheme_eval(vals[i],env))
         i +=1
     return vals[i-1] # Return last value of list, or return false value
 
