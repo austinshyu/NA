7333188ba855e9cd21343692251b38e0362adbd1
BoYoungHwang <by.hwang@berkeley.edu>
2013-11-19 19:29:59 -0800

    q22 added

--- scheme.py
+++ scheme.py
@@ -435,7 +435,7 @@ def scheme_optimized_eval(expr, env):
         # Evaluate Combinations
         if (scheme_symbolp(first) # first might be unhashable
             and first in LOGIC_FORMS):
-            "*** YOUR CODE HERE ***"
+            expr = LOGIC_FORMS[first](rest,env)
         elif first == "lambda":
             return do_lambda_form(rest, env)
         elif first == "mu":
@@ -445,14 +445,26 @@ def scheme_optimized_eval(expr, env):
         elif first == "quote":
             return do_quote_form(rest)
         elif first == "let":
-            "*** YOUR CODE HERE ***"
+            expr,env = do_let_form(rest,env)
         else:
-            "*** YOUR CODE HERE ***"
+            procedure = scheme_optimized_eval(first, env)
+            args = rest.map(lambda operand: scheme_optimized_eval(operand, env))
+            if isinstance(procedure, PrimitiveProcedure):
+                return apply_primitive(procedure, args, env)
+            elif isinstance(procedure, LambdaProcedure):
+                expr = procedure.body
+                env = procedure.env.make_call_frame(procedure.formals,args)
+            elif isinstance(procedure, MuProcedure):
+                expr = procedure.body
+                env = env.make_call_frame(procedure.formals, args)
+            else:
+                raise SchemeError("Cannot call {0}".format(str(procedure)))
+
 
 ################################################################
 # Uncomment the following line to apply tail call optimization #
 ################################################################
-# scheme_eval = scheme_optimized_eval
+scheme_eval = scheme_optimized_eval
 
 
 ################
